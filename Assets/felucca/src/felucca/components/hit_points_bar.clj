49474ace90ec95eff829646d83fd6b16cd475067
Murilo Pereira <murilo@murilopereira.com>
2020-01-09 00:57:10 +0100

    Border for hit points bar.

--- Assets/felucca/src/felucca/components/hit_points_bar.clj
+++ Assets/felucca/src/felucca/components/hit_points_bar.clj
@@ -6,15 +6,19 @@
             Renderer ScaleMode TextureFormat]))
 
 (defmutable HitPointsBar [^Texture2D hit-points-bar-texture-background
-                          ^Texture2D hit-points-bar-texture])
+                          ^Texture2D hit-points-bar-texture
+                          ^Texture2D hit-points-bar-texture-border])
 
 (defn start! [^GameObject this role-key]
   (let [hit-points-bar-texture (Texture2D. 1 1 (TextureFormat/RGB24) false)
-        hit-points-bar-texture-background (Texture2D. 1 1 (TextureFormat/RGB24) false)]
-    (.SetPixel hit-points-bar-texture-background 0 0 (Color/black))
+        hit-points-bar-texture-background (Texture2D. 1 1 (TextureFormat/RGB24) false)
+        hit-points-bar-texture-border (Texture2D. 1 1 (TextureFormat/RGB24) false)]
+    (.SetPixel hit-points-bar-texture-background 0 0 (Color/red))
     (.Apply hit-points-bar-texture-background)
-    (.SetPixel hit-points-bar-texture 0 0 (Color/red))
+    (.SetPixel hit-points-bar-texture 0 0 (Color/blue))
     (.Apply hit-points-bar-texture)
+    (.SetPixel hit-points-bar-texture-border 0 0 (Color/gray))
+    (.Apply hit-points-bar-texture-border)
     (update-state this
                   :hit-points-bar
                   (fn [hit-points-bar-state]
@@ -22,7 +26,9 @@
                            :hit-points-bar-texture-background
                            hit-points-bar-texture-background
                            :hit-points-bar-texture
-                           hit-points-bar-texture)))))
+                           hit-points-bar-texture
+                           :hit-points-bar-texture-border
+                           hit-points-bar-texture-border)))))
 
 (defn on-gui! [^GameObject this role-key]
   (let [hit-points-bar-state (state this :hit-points-bar)
@@ -32,18 +38,22 @@
                (.. r bounds size))
         view-position (.. Camera main (WorldToScreenPoint local-position))
         border 2
-        background-width 80
-        width (* background-width (creature/hit-points-percentage this))
+        width 80
         height 6
-        x (- (.x view-position) (/ background-width 2))
+        hit-points-percentage-width (* width
+                                       (creature/hit-points-percentage this))
+        x (- (.x view-position) (/ width 2))
         y (- (Screen/height) (.y view-position) (* 25 (.y size)))]
     (.. GUI (DrawTexture (Rect. (- x border)
                                 (- y border)
-                                (+ background-width (* 2 border))
+                                (+ width (* 2 border))
                                 (+ height (* 2 border)))
-                         (:hit-points-bar-texture-background hit-points-bar-state)
+                         (:hit-points-bar-texture-border hit-points-bar-state)
                          (ScaleMode/StretchToFill)))
     (.. GUI (DrawTexture (Rect. x y width height)
+                         (:hit-points-bar-texture-background hit-points-bar-state)
+                         (ScaleMode/StretchToFill)))
+    (.. GUI (DrawTexture (Rect. x y hit-points-percentage-width height)
                          (:hit-points-bar-texture hit-points-bar-state)
                          (ScaleMode/StretchToFill)))))
 
